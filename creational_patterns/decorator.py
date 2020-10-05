08f7a3cdb1717e1ac68eb088d90eaca231b09adf
Lomakin Sergey <71843430+sirejik@users.noreply.github.com>
2020-10-05 23:27:33 +0700

    Add decorator pattern

---
+++ creational_patterns/decorator.py
@@ -0,0 +1,26 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Component(metaclass=ABCMeta):
+    @abstractmethod
+    def operation(self):
+        pass
+
+
+class ConcreteComponent(Component):
+    def operation(self):
+        print('ConcreteComponent')
+
+
+class Decorator(Component):
+    def __init__(self, component: Component):
+        self.component = component
+
+    def operation(self):
+        print('Decorator')
+        self.component.operation()
+
+
+concrete_component = ConcreteComponent()
+concrete_component = Decorator(concrete_component)
+concrete_component.operation()
