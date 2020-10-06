3cde76648ba83222e6402ba5fe9d4dca6d7c4fb6
Lomakin Sergey <71843430+sirejik@users.noreply.github.com>
2020-10-06 23:30:07 +0700

    Add facade pattern

---
+++ structural_patterns/facade.py
@@ -0,0 +1,33 @@
+class FooClass:
+    @staticmethod
+    def foo1():
+        print('foo1')
+
+    @staticmethod
+    def foo2():
+        print('foo2')
+
+
+class BarClass:
+    @staticmethod
+    def bar1():
+        print('bar1')
+
+    @staticmethod
+    def bar2():
+        print('bar2')
+
+
+class Facade:
+    def __init__(self):
+        self.foo_class = FooClass()
+        self.bar_class = BarClass()
+
+    def foo(self):
+        self.foo_class.foo1()
+        self.foo_class.foo2()
+        self.bar_class.bar1()
+
+
+facade = Facade()
+facade.foo()
